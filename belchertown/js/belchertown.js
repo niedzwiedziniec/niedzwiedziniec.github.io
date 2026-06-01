aa62d9815c0110eb7af50b6be634e3da50180802
niedzwiedziniec <franio94@gmail.com>
2026-06-02 00:10:01 +0200

    Automatyczna aktualizacja: 2026-06-02 00:10

--- belchertown/js/belchertown.js
+++ belchertown/js/belchertown.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function() {
         } else if (window.location.search.indexOf('?theme=auto') === 0) {
             belchertown_debug("Theme: Setting auto theme because of URL override");
             sessionStorage.setItem('theme', 'auto')
-            autoTheme(20, 47, 04, 40)
+            autoTheme(20, 48, 04, 39)
         }
     }
 
